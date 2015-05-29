d4bbbbdc48315ecb5f7b586264eaf5d37653d859
Rainer Hermanns <hermanns@aixcept.de>
2015-05-29 17:32:12 +0200

    updated

---
+++ modules/postgres/postgresql.zsh
@@ -0,0 +1,10 @@
+#-------------------------
+# DATABASE CODING
+#-------------------------
+
+#-------------------------
+# PostgreSQL
+#-------------------------
+
+export POSTGRESQL_HOME=/Library/PostgreSQL/9.3
+export PATH=$POSTGRESQL_HOME/bin:$PATH
